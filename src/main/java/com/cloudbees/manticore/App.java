ad5688d9dc73d9641f2089bb0eafb8139e38b13c
Prasad <prasad.durga1@wipro.com>
2017-09-08 14:42:03 +0100

    hi

---
+++ src/main/java/com/cloudbees/manticore/App.java
@@ -0,0 +1,20 @@
+package com.cloudbees.manticore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void in( String[] args )
+    {
+        String a = null;
+        System.out.println(a.toString());
+        System.out.println( "Hello World!" );
+        // TODO: this is a task
+        // FIXME: this is another task 
+        try {
+            a = "Hello";
+        } catch (Exception e) {}
+    }
+}
